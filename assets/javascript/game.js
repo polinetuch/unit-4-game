ed8567c6051e6237d67aeda6f4f75358c8accf6b
unknown <uchpolinet@gmail.com>
2019-07-07 23:13:10 +1000

    added comments to what each code does when displayed in browser

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
 
   // create function to reset the button and random number values
   function restartTheGame() {
+    // generate a random number from 19 to 120
+    randomTargetNumber = Math.floor(Math.random() * 102) + 19;
+    userGussedNumber = 0;
+
     // image buttons to generate random number from 1 to 12
     $(".btnImage01").val(Math.floor(Math.random() * 12) + 1);
     $(".btnImage02").val(Math.floor(Math.random() * 12) + 1);
@@ -28,24 +32,31 @@ $(document).ready(function() {
 
   // when player presses a key, add the random value generated to the HTML element
   $(".styleImages").on("click", function() {
+    // to pass the random number to user guess - the value is generated from the random math floor
     userGussedNumber = userGussedNumber + parseInt($(this).val());
     $(".displayValueFromUserGuess").text("Your Guess: " + userGussedNumber);
     userGussedNumber = userGussedNumber + parseInt($(this).val());
     $(".displayValueFromUserGuess").text("Your Guess: " + userGussedNumber);
 
-    //if player wins, increase the wins counter and display message Well done!
+    //if player wins
     if (userGussedNumber === randomTargetNumber) {
-      // display the number of wins after guessing
+      // display the number of wins after guessing and increases the wins counter
       winsCounter++;
+
+      // display message Well done!
       $(".displayWinOrLoseText").text("Well done!");
+
       // call the function to restart the game according to what is stated in the function
       restartTheGame();
 
-      // if the player losses, increase the losses counter and display message Try again later!
+      // if the player losses
     } else if (userGussedNumber > randomTargetNumber) {
-      // display the number of losses after guessing
+      // display the number of losses after guessing and increases the losses counter
       lossesCounter++;
+
+      // display message Try again later!
       $(".displayWinOrLoseText").text("Try again later!");
+
       // call the function to restart the game according to what is stated in the function
       restartTheGame();
     }
